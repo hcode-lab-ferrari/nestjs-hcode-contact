06bb815134680b1ab60bd257310368923cc80c2a
João Rangel <joaohcrangel@gmail.com>
2022-03-01 21:44:59 -0300

    ok

--- src/hcode-contact.module.ts
+++ src/hcode-contact.module.ts
@@ -3,10 +3,8 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { HcodeContactService } from './hcode-contact.service';
 
 @Module({
-  imports: [
-    PrismaModule,
-  ],
-  providers: [HcodeContactService],
-  exports: [HcodeContactService],
+    imports: [PrismaModule],
+    providers: [HcodeContactService],
+    exports: [HcodeContactService],
 })
 export class HcodeContactModule {}
--- src/hcode-contact.service.ts
+++ src/hcode-contact.service.ts
@@ -1,31 +1,30 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class HcodeContactService {
-
     constructor(private db: PrismaService) {}
 
     async get(id: number) {
-
         id = Number(id);
 
         if (isNaN(id)) {
-            throw new BadRequestException("ID is not a number");
+            throw new BadRequestException('ID is not a number');
         }
 
         return this.db.contact.findUnique({
             where: {
                 id,
-            }
+            },
         });
-
     }
 
     async list() {
-
         return this.db.contact.findMany();
-
     }
 
     async create({
@@ -37,13 +36,12 @@ export class HcodeContactService {
         email: string;
         message: string;
     }) {
-
         if (!email) {
-            throw new BadRequestException("O e-mail é obrigatório.");
+            throw new BadRequestException('O e-mail é obrigatório.');
         }
 
         if (!message) {
-            throw new BadRequestException("A mensagem é obrigatória.");
+            throw new BadRequestException('A mensagem é obrigatória.');
         }
 
         let personId: number;
@@ -60,7 +58,6 @@ export class HcodeContactService {
         if (user) {
             personId = Number(user.personId);
         } else {
-
             const contact = await this.db.contact.findFirst({
                 where: {
                     email,
@@ -70,17 +67,14 @@ export class HcodeContactService {
             if (contact) {
                 personId = Number(contact.personId);
             } else {
-
                 const newPerson = await this.db.person.create({
                     data: {
                         name,
                     },
                 });
-    
+
                 personId = Number(newPerson.id);
-
             }
-
         }
 
         return this.db.contact.create({
@@ -90,27 +84,23 @@ export class HcodeContactService {
                 message,
             },
         });
-
     }
 
     async delete(id: number) {
-
         id = Number(id);
 
         if (isNaN(id)) {
-            throw new BadRequestException("Id is invalid");
+            throw new BadRequestException('Id is invalid');
         }
 
-        if (!await this.get(id)) {
-            throw new NotFoundException("ID not exists");
+        if (!(await this.get(id))) {
+            throw new NotFoundException('ID not exists');
         }
-        
+
         return this.db.contact.delete({
             where: {
                 id,
-            }
+            },
         });
-
     }
-
 }
